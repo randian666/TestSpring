89e760cb7de16144e169051aad6fc55255134a6d
liuxun <liuxun@jd.com>
2017-08-31 18:41:39 +0800

    springmvc+velocity集成

--- SpringMvcDemo/src/main/java/com/springmvc/demo/controller/PerformanceController.java
+++ SpringMvcDemo/src/main/java/com/springmvc/demo/controller/PerformanceController.java
@@ -1,5 +1,6 @@
 package com.springmvc.demo.controller;
 
+import com.springmvc.demo.service.CompactDisc;
 import com.springmvc.demo.service.Performance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
+ * AOP测试
  * Created by liuxun on 2017/8/17.
  */
 @Controller
@@ -15,11 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PerformanceController {
     @Autowired
     private Performance jayPerformance;
+    @Autowired
+    private CompactDisc gtPepperss;
     @RequestMapping(value = "/doPer",method = RequestMethod.GET)
     public String doPer(String name,ModelMap model) {
         model.addAttribute("message", "Jay");
         jayPerformance.perform();
         jayPerformance.performByName(name);
+        return "hello";
+    }
+
+    @RequestMapping(value = "/doPep",method = RequestMethod.GET)
+    public String doPep(String name,ModelMap model) {
+        model.addAttribute("message", "Jay");
+        gtPepperss.play();
         return "hello";
     }
 }
